bcbe731a5b1eb5d72bf09487d5162439779a037f
Jitesh <jiteshpal2604@gmail.com>
2026-03-26 13:14:31 +0530

    fixed css

--- client/src/components/shared/ScrollReveal.tsx
+++ client/src/components/shared/ScrollReveal.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState, ReactNode } from "react";
+import { useEffect, useRef, useState } from "react";
+import type { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 
 interface ScrollRevealProps {
