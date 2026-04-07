c78ad79c606b7f8c60fe3828ddeede50e4702f55
Jitesh <jiteshpal2604@gmail.com>
2026-04-07 15:22:35 +0530

    added authRoute

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
+import authRoutes from './routes/authRoutes.js';
+import { authMiddleware } from './middlewares/authMiddleware';
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.use('/api/auth', authMiddleware,  authRoutes);
 
 app.listen(PORT, async () => {
     try {
